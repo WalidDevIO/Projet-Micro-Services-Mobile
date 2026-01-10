c98765f53d3fa7d83f27766f95fe8292bbb1b110
WalidDevIO <admin@walidlab.dev>
2026-01-10 15:24:45 +0100

    feat: Log on FunctionalExceptionHandler

--- EcommAPI/src/main/java/com/example/ubo/ecommapi/handlers/FunctionalExceptionHandler.java
+++ EcommAPI/src/main/java/com/example/ubo/ecommapi/handlers/FunctionalExceptionHandler.java
@@ -1,16 +1,23 @@
 package com.example.ubo.ecommapi.handlers;
 
 import com.example.ubo.ecommapi.exceptions.FunctionalException;
+import dto.ecommapi.Error;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import java.util.logging.Logger;
 
 @Provider
 public class FunctionalExceptionHandler implements ExceptionMapper<FunctionalException> {
+    private static final Logger logger = Logger.getLogger(FunctionalExceptionHandler.class.getName());
+
     @Override
     public Response toResponse(FunctionalException e) {
+        logger.warning(e.getCode() + ": " + e.getMessage());
+
         return Response
                 .status(400)
-                .entity("TODO: Use error class").build(); //TODO: User error DTO class
+                .entity(new Error().code(e.getCode()).message(e.getMessage()))
+                .build();
     }
 }
