ce5e96ee56c7db749ef04f06468f769eaa67620b
WalidDevIO <admin@walidlab.dev>
2026-01-10 15:59:47 +0100

    misc: Dockerized expo app for demo

--- ecomm-mobile-app/constants/api.js
+++ ecomm-mobile-app/constants/api.js
@@ -1,6 +1,8 @@
+import Constants from 'expo-constants';
+
 export const API_CONFIG = {
-  AUTH_API_URL: 'http://192.168.1.5:8080/api/v1',
-  ECOMM_API_URL: 'http://192.168.1.5:8081/api/v1',
+  AUTH_API_URL: Constants.expoConfig?.extra?.authApiUrl || 'http://localhost:8080/api/v1',
+  ECOMM_API_URL: Constants.expoConfig?.extra?.ecommApiUrl || 'http://localhost:8081/api/v1',
   TIMEOUT: 10000,
 };
 
